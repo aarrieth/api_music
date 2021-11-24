7d03019481f4fcd880d585772c9e432a99fe3e0f
aarrieth <alfonsoluisarrieta253@gmail.com>
2021-11-24 17:27:49 -0500

    [ADD] function to get full URL]

--- src/index.js
+++ src/index.js
@@ -8,23 +8,15 @@ const PORT = 3001
 app.use(helmet())
 app.use(express.json()) //para obtener el objeto body de la request en formato JSON
 
-app.get('/', (req, res)=> {
+app.get('/', async (req, res)=> {
+    const fullUrl =
+    (await req.protocol) + '://' + req.get('Host') + req.originalUrl;
     res.status(200).json({
-        "Discografia Diomedez Diaz": `http://localhost:${PORT}/api/v1/music/diomedes-diaz`,
+        "Discografia Diomedez Diaz": `${fullUrl}api/v1/music/diomedes-diaz`,
     })
 })
 
 routesOfMusic(app)
-
-/* 
-
-app.delete('/api/v1/colombian-music/diomedes-diaz/:id', (req, res)=>{
-    const id = parseInt(req.params.id);
-    const disc = api.filter(item => item.id != id);
-    res.status(204).end()
-})
-
- */
 
 app.listen(PORT, ()=> {
     console.log(`Server run on: http://localhost:${PORT}`);
